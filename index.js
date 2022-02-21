7939103d13ac6777ea943e08f16247480c512484
gulnaez <213023@astanait.edu.kz>
2022-02-21 22:41:51 +0600

    commit 2

--- index.js
+++ index.js
@@ -6,6 +6,155 @@ body.style.textDecoration = "none";
 var title = document.getElementById("title");
 title.style.paddingTop = "2%";
 
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
+
+
+$(document).ready(function(){
+  $("#home").mouseover(function(){
+    $("#home").css("color", "black").css("font-weight", "bold");
+  });
+  $("#home").mouseout(function(){
+    $("#home").css("color", "gray").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#about").mouseover(function(){
+    $("#about").css("color", "black").css("font-weight", "bold");
+  });
+  $("#about").mouseout(function(){
+    $("#about").css("color", "gray").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#sections").mouseover(function(){
+    $("#sections").css("color", "black").css("font-weight", "bold");
+  });
+  $("#sections").mouseout(function(){
+    $("#sections").css("color", "gray").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#products").mouseover(function(){
+    $("#products").css("color", "black").css("font-weight", "bold");
+  });
+  $("#products").mouseout(function(){
+    $("#products").css("color", "gray").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#home1").mouseover(function(){
+    $("#home1").css("color", "black").css("font-weight", "bold");
+  });
+  $("#home1").mouseout(function(){
+    $("#home1").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#about1").mouseover(function(){
+    $("#about1").css("color", "black").css("font-weight", "bold");
+  });
+  $("#about1").mouseout(function(){
+    $("#about1").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#sections1").mouseover(function(){
+    $("#sections1").css("color", "black").css("font-weight", "bold");
+  });
+  $("#sections1").mouseout(function(){
+    $("#sections1").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#products1").mouseover(function(){
+    $("#products1").css("color", "black").css("font-weight", "bold");
+  });
+  $("#products1").mouseout(function(){
+    $("#products1").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#ins").mouseover(function(){
+    $("#ins").css("color", "black").css("font-weight", "bold");
+  });
+  $("#ins").mouseout(function(){
+    $("#ins").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#tik").mouseover(function(){
+    $("#tik").css("color", "black").css("font-weight", "bold");
+  });
+  $("#tik").mouseout(function(){
+    $("#tik").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#twit").mouseover(function(){
+    $("#twit").css("color", "black").css("font-weight", "bold");
+  });
+  $("#twit").mouseout(function(){
+    $("#twit").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+$(document).ready(function(){
+  $("#kumis").mouseover(function(){
+    $("#kumis").css("color", "black").css("font-weight", "bold");
+  });
+  $("#kumis").mouseout(function(){
+    $("#kumis").css("color", "black").css("font-weight", "normal");
+  });
+});
+
+j = 0;
+$(document).ready(function(){
+  $("#p1").keypress(function(){
+    $("#num").text(j += 1);
+});
+  $("#btn").click(function(){
+    $("#p1").keypress();
+  });
+});
+
+$(document).ready(function(){
+  $("#buy").click(function(){
+    $("#buy").click();
+  });
+});
+
 document.getElementById("product").style.margin = "2% 2% 10% 2%";
 
 var elementsh1 = document.getElementsByTagName("h1");
@@ -58,34 +207,3 @@ var cardText = document.getElementsByClassName('card-text');
 for (var i = 0; i < cardText.length; i++) {
   cardText[i].style.textAlign = "center";
 }
-
-const product1 = {
-  title: "Köz set",
-  price: "5000₸",
-};
-
-product1.madefrom = "silver";
-product1.inStock = 30;
-
-delete product1.madefrom;
-
-console.log(product1);
-
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml16');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml16 .letter',
-    translateY: [-100,0],
-    easing: "easeOutExpo",
-    duration: 1400,
-    delay: (el, i) => 30 * i
-  }).add({
-    targets: '.ml16',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
